2ae25f26412b9e4077fcd670011e1d86179b0326
AquiburRahmanKhan <aquibur.rahman@monstar-lab.com>
2024-08-13 12:26:32 +0600

    feat: removed log for database location

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,6 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  console.log(
-    "directive name",
-    path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db"))
-  );
   return {
     connection: {
       client: "sqlite",
