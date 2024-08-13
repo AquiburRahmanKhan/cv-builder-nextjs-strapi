2347f9cf5d905958f4836cd616af5a9e7395e5da
AquiburRahmanKhan <aquibur.rahman@monstar-lab.com>
2024-08-13 12:42:50 +0600

    feat: added log for database location

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,17 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
+  console.log(
+    "directive name",
+    path.join(__dirname, "..", "..", env("DATABASE_FILENAME", ".tmp/data.db"))
+  );
   return {
     connection: {
       client: "sqlite",
       connection: {
         filename: path.join(
           __dirname,
+          "..",
           "..",
           env("DATABASE_FILENAME", ".tmp/data.db")
         ),
