031daae99bc37b5d1e064740e623bde60d2a35a6
AquiburRahmanKhan <aquibur.rahman@monstar-lab.com>
2024-08-13 13:03:56 +0600

    fix: fixed database connection in prod

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,21 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  console.log(
-    "directive name",
-    path.join(__dirname, "..", "..", env("DATABASE_FILENAME1", ".tmp/data.db"))
-  );
-  return {
-    connection: {
-      client: "sqlite",
-      connection: {
-        filename: path.join(
+  const filename =
+    env("NODE_ENV") === "production"
+      ? path.join(
           __dirname,
           "..",
           "..",
           env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
+        )
+      : path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db"));
+
+  return {
+    connection: {
+      client: "sqlite",
+      connection: {
+        filename: filename,
       },
       useNullAsDefault: true,
     },
